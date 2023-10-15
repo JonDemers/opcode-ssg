5ef7dda7db93340b89072c5f93d1a38eac47be93
Jonathan Demers <jdemers@opcodesolutions.com>
2023-10-15 13:43:58 -0400

    Remove debug commands

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -9,7 +9,3 @@ bundle exec jekyll clean && bundle exec jekyll build
 rm -rf ../opcodesolutions.github.io/*
 
 cp -r _site/* ../opcodesolutions.github.io/
-
-rm -rf /mnt/c/Users/demer/git/opcodesolutions.github.io-v2
-
-cp -r _site /mnt/c/Users/demer/git/opcodesolutions.github.io-v2
