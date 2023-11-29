66189da52bea23dee3e11b041abbddde26bea938
Jonathan Demers <jdemers@opcodesolutions.com>
2023-11-29 06:50:52 -0500

    Support old wordpress backlink

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -9,3 +9,8 @@ bundle exec jekyll clean && bundle exec jekyll build
 rm -rf ../opcodesolutions.github.io/*
 
 cp -r _site/* ../opcodesolutions.github.io/
+
+# We have some "<a href='...'" backlinks to this old image from old wordpress website that no longer exists.
+# We'll just serve the same (duplicate) html content. We have "rel='canonical'" to the main page.
+mkdir -p ../opcodesolutions.github.io/tech/wp-content/uploads/2014/11/2014-11-04-143434_714x503_scrot.png
+cp _site/tech/solve-java-lang-outofmemoryerror-java-heap-space/* ../opcodesolutions.github.io/tech/wp-content/uploads/2014/11/2014-11-04-143434_714x503_scrot.png
